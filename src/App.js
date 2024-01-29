6ef0c1e2a67e361de1d2abdad7c2e6b416c18a53
Varvara Korableva <v.korableva1990@yandex.ru>
2024-01-29 17:11:29 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react'
-//import { Route, Routes } from 'react-router-dom'
-
 import './App.css';
-/*import Header from './Components/Header/Header'
-import MyStrongSides from './Components/MyStrongSides/MyStrongSides'
-import About from './Components/About/About'
-import Stack from './Components/Stack/Stack'
-import Footer from './Components/Footer/Footer'
-import WorkExperience from './Components/WorkExperience/WorkExperience'
-import Education from './Components/Education/Education'
-import ProjectContainer from './Components/ProjectContainer/ProjectContainer'*/
+import Header from './components/Header/Header'
+import MyStrongSides from './components/MyStrongSides/MyStrongSides'
+import About from './components/About/About'
+import Stack from './components/Stack/Stack'
+import Footer from './components/Footer/Footer'
+import WorkExperience from './components/WorkExperience/WorkExperience'
+import Education from './components/Education/Education'
+import ProjectContainer from './components/ProjectContainer/ProjectContainer'
 
 function App() {
   return (
     <div className="App"> 
-      <h1>Varvara Korableva</h1>
-    </div>
-  );
-}
-
-export default App;
-
-
-/*
       <Header></Header>
       <MyStrongSides/>
       <Stack></Stack>
@@ -31,4 +20,8 @@ export default App;
       <Education></Education>
       <About></About>
       <Footer></Footer>
-*/
+    </div>
+  );
+}
+
+export default App;
